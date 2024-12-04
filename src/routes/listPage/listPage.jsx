2642189224562b8ac666705a89575e673b7167bd
Shair-Ali1107 <sher48001@gmail.com>
2024-12-04 12:55:10 +0500

    project initialized

---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,15 @@
+import "./listPage.scss";
+
+
+function ListPage() {
+  const data = listData;
+
+  return <div className="listPage">
+    <div className="listContainer">
+      <div className="wrapper">
+      </div>
+    </div>
+  </div>;
+}
+
+export default ListPage;
